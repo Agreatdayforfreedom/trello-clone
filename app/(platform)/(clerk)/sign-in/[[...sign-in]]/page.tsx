68453814a7141396fe7e67d36e514ba2141a03a4
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-03-21 16:56:32 -0300

    test user

--- app/(platform)/(clerk)/sign-in/[[...sign-in]]/page.tsx
+++ app/(platform)/(clerk)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,44 @@
-import { SignIn } from "@clerk/nextjs";
-
+"use client";
+import { SignIn, useSignIn } from "@clerk/nextjs";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 export default function Page() {
-  return <SignIn />;
+	const searchParams = useSearchParams();
+	const router = useRouter();
+	const { isLoaded, signIn, setActive } = useSignIn();
+
+	const handleSubmit = async () => {
+		if (!isLoaded) {
+			return;
+		}
+		try {
+			console.log("HERE");
+
+			const completeSignIn = await signIn.create({
+				identifier: process.env.NEXT_PUBLIC_TEST_USERNAME! as string,
+				password: process.env.NEXT_PUBLIC_TEST_PASSWORD! as string,
+			});
+
+			if (completeSignIn.status !== "complete") {
+				console.log(JSON.stringify(completeSignIn, null, 2));
+			}
+
+			if (completeSignIn.status === "complete") {
+				await setActive({ session: completeSignIn.createdSessionId });
+				router.push("/");
+			}
+		} catch (err: any) {
+			console.error(JSON.stringify(err, null, 2));
+		}
+	};
+	useEffect(() => {
+		async function go() {
+			if (searchParams.get("mode") === "test") {
+				await handleSubmit();
+			}
+		}
+		go();
+	}, [searchParams, isLoaded]);
+
+	return <SignIn />;
 }
