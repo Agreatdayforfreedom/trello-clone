056e173d7b6e13eac8777da6e6e2cecedabbfdf8
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-03-21 11:32:00 -0300

    fix typo

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/form.tsx
@@ -6,18 +6,17 @@ import { FormInput } from "@/components/form/form-input";
 import { FormSubmit } from "@/components/form/form-submit";
 
 export const Form = () => {
-	const { execute, fieldsErrors } = useAction(createBoard);
+	const { execute, fieldErrors } = useAction(createBoard);
 
 	const onSubmit = (formData: FormData) => {
 		const title = formData.get("title") as string;
 		execute({ title });
 	};
-	console.log(fieldsErrors);
 
 	return (
 		<form action={onSubmit}>
 			<div>
-				<FormInput id="title" label="Board title" errors={fieldsErrors} />
+				<FormInput id="title" label="Board title" errors={fieldErrors} />
 			</div>
 			<FormSubmit>Save</FormSubmit>
 		</form>
